8c573d13adbdddaf6e273b027a39fb48ea0ced95
Mathews Alufwani <mathewalufwani@gmail.com>
2020-04-22 12:14:43 +0300

    add form function

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function Pdetails(fname, lname, number, email, cname,sname){
+  this.fname = fname;
+  this.lname = lname;
+  this.number = number;
+  this.email = email;
+  this.cname = cname;
+  this.sname = sname;
+}
+
+function displayDelivery() {
+  $(".addressInput").show(1000);
+  $(".noDelivery").hide(1000);
+}
